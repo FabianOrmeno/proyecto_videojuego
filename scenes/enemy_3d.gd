44457adb435887c0fe4a195c3db06c5d1a786096
damson999 <gonzo2077.rod@gmail.com>
2026-06-03 11:19:57 -0400

    Se activa el jumpscare

--- scenes/enemy_3d.gd
+++ scenes/enemy_3d.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 @export var speed = 1
 @export var stun_time = 2.0
-@export var catch_distance = 2.0
+@export var catch_distance = 3.0
 @export var chase_distance = 15.0
 @export var player : CharacterBody3D
 
@@ -12,6 +12,8 @@ extends CharacterBody3D
 
 var stun_timer = 0.0
 
+func _ready():
+	animation_player.play("enemy_animaciones/walk")
 
 func _physics_process(delta: float) -> void:
 	if player == null:
@@ -53,9 +55,10 @@ func _physics_process(delta: float) -> void:
 	velocity.z = direction.z * speed
 	
 	move_and_slide()
-	Debug.log(velocity.length(),2*delta)
 	animation_player.speed_scale=velocity.length()/2
 	if global_position.distance_to(player.global_position) <= catch_distance:
+		animation_player.play("enemy_animaciones/jumpscare")
+		await animation_player.animation_finished
 		get_tree().paused = true
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
