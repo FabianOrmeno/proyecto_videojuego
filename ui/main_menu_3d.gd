f791df6286e76f8c46bee0f60eb8162b3cce50a4
cadwalch <marce6234@gmail.com>
2026-07-06 00:03:24 -0400

    feat: se añaden los creditos en el juego

--- ui/main_menu_3d.gd
+++ ui/main_menu_3d.gd
@@ -52,3 +52,6 @@ func _on_button_play_pressed() -> void:
 
 func _on_button_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_button_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://ui/credits_3d.tscn")
