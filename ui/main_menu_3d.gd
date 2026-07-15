2a7187aacfffcebedc7643cc52c5c51b2cb112bd
damson999 <gonzo2077.rod@gmail.com>
2026-07-14 21:52:15 -0400

    Se agrega el tutorial al menu

--- ui/main_menu_3d.gd
+++ ui/main_menu_3d.gd
@@ -52,3 +52,6 @@ func _on_button_play_pressed() -> void:
 
 func _on_button_exit_pressed() -> void:
 	get_tree().quit()
+
+func _on_button_tutorial_pressed() -> void:
+	get_tree().change_scene_to_file("res://ui/tutorial_3d.tscn")
